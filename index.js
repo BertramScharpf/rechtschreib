3e367bc6b6564088d89649a45c357f98b411c6cc
Bertram Scharpf <software@bertram-scharpf.de>
2015-09-21 21:18:33 +0200

    Standard-Taster hinzugefügt

--- index.js
+++ index.js
@@ -3,13 +3,31 @@
 //
 
 var self = require('sdk/self');
+var tabs = require("sdk/tabs");
 
-require("sdk/tabs").on( "ready", runScript);
 
-
+tabs.on( "ready", runScript);
 function runScript(tab) {
   tab.attach({
     contentScriptFile: self.data.url("saeubern.js")
   });
+}
+
+
+var buttons = require('sdk/ui/button/action');
+
+var button = buttons.ActionButton({
+  id: "bsch-link",
+  label: "Visit Bertram Scharpf",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick(state) {
+  tabs.open( "http://www.bertram-scharpf.de");
 }
 
