de4a417cc14ec9e4e147d2561b803bbcbe6ffbd1
Bertram Scharpf <software@bertram-scharpf.de>
2015-09-21 02:05:06 +0200

    Erstfassung

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 #Rechtschreibung
-Reformierte Rechtschreibung entfernen
+
+This tool removes the ugly so-called „reformed“ orthography
+from web contents. As this is a concern only for readers of
+German texts, the rest of the documentation will be written
+in German.
+
+
+Dieses Modul entfernt die sogenannte „reformierte“ Rechtschreibung
+von jeder aufgerufenen Netzseite.
+
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//
+//  index.js  --  Eingangspunkt
+//
+
 var self = require('sdk/self');
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+require("sdk/tabs").on( "ready", runScript);
+
+
+function runScript(tab) {
+  tab.attach({
+    contentScriptFile: self.data.url("saeubern.js")
+  });
 }
 
-exports.dummy = dummy;
