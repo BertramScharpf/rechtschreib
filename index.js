da8acd5842b8156cc546a1d4311e0867383f987c
Bertram Scharpf <software@bertram-scharpf.de>
2015-09-23 23:05:39 +0200

    Taster wechselt die Farbe

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 //  index.js  --  Eingangspunkt
 //
 
+
 var self = require('sdk/self');
+
+saeubere = true;
+
+
 var tabs = require("sdk/tabs");
-
-
 tabs.on( "ready", runScript);
 function runScript(tab) {
   tab.attach({
@@ -14,20 +17,29 @@ function runScript(tab) {
 }
 
 
+normal_icons = {
+  "16": "./gruen-16.png",
+  "32": "./gruen-32.png",
+  "64": "./gruen-64.png"
+};
+inactive_icons = {
+  "16": "./grau-16.png",
+  "32": "./grau-32.png",
+  "64": "./grau-64.png"
+};
+
+
 var buttons = require('sdk/ui/button/action');
 
 var button = buttons.ActionButton( {
-  id: "bsch-link",
-  label: "Visit Bertram Scharpf",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
+  id: "rechtschreibung",
+  label: "Rechtschreibung säubern",
+  icon: normal_icons,
   onClick: handleClick
 });
 
 function handleClick( state) {
-  tabs.open( "http://www.bertram-scharpf.de");
+  saeubere = !saeubere;
+  button.state( "window", { icon: saeubere ? normal_icons : inactive_icons});
 }
 
