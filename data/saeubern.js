b8f369eb93f64f3d777485ec353d265b57fbff49
Bertram Scharpf <software@bertram-scharpf.de>
2015-09-26 20:45:11 +0200

    Bessere Erkennung

--- data/saeubern.js
+++ data/saeubern.js
@@ -86,14 +86,14 @@ function saeubereString( s) {
 
     r = r.replace( /([bdfgklmnprt])\1\1(?=[aeiouyäöü])/g, "$1$1");
     r = r.replace( /sss/g, "ßs");
-    r = r.replace( /((?:^|\s)(?:[AEIOUÄÖÜaeiouäöü]))ss[^aeiouäöü]/g, "$1ß");
+    r = r.replace( /((?:^|\s)(?:[AEIOUÄÖÜaeiouäöü]))ss(?![aeiouäöü])/g, "$1ß");
     r = r.replace( /((?:^|\s)(?:[BbFfNnPp]a|[Mm][eiü]|[Rr][ou]|[FfGg]u))ss(?!e[lmnrst]?\b|ung|ist|isch|ion)/g, "$1ß");
-    r = r.replace( /((?:^|\s)(?!Frä|G[rl]a|I[br]i)[A-Za-zäöüÄÖÜ](?:[fnlprt][aeiouäöüy]))ss(?!e[lmnrst]?\b|ung|ist|isch|ion)/g, "$1ß");
+    r = r.replace( /((?:^|\s)(?!Frä|G[rl]a|I[br]i)[A-Za-zäöüÄÖÜ](?:[fnlprt][aeiouäöüy]))ss(?!e[lmnrst]?\b|ung|ist|isch|ion|il)/g, "$1ß");
     r = r.replace( /((?:^|\s)(?:[ABD-ZÄÖÜabd-zäöüß]|ch|ck)+)ss(t(?:e[rnms])?)?\b/g, "$1ß$2");
     r = r.replace( /([a-zäöü])ss(?=[bdfghjklmnqrvwxz]\w*[aeiouyäöü]|c(?:[^h]|h(?:en\b|arakt|emi[eks]|irurg)))/g, "$1ß");
     r = r.replace( /ssp(?=f|re(?:ch|ng|i)|l[^i])/g, "ßp");
 
-    r = r.replace( /((?:[Ss]ch|[Ff])l?[uo])ss(?!e[lmnrst]?)/g, "$1ß");
+    r = r.replace( /((?:[Ss]ch|[Ff])l?[uo])ss(?!e[lmnrst]?|il)/g, "$1ß");
     r = r.replace( /(pa)ss(?=w[oö]rt|phrase)/g, "$1ß");
     r = r.replace( /([Aa]dre)ss(?!e|at)/g, "$1ß");
     r = r.replace( /([Pp]roze)ss(?!e|or)/g, "$1ß");
